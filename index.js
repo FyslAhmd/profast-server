68b5a4d041be1ae7a1e66b4e7a01b32451e1d2b7
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-04 17:11:57 +0600

    implement stripe payment api, get and post api for payment history and others

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -22,6 +23,12 @@ async function run() {
   try {
     await client.connect();
     const parcelsCollection = client.db("parcelService").collection("parcels");
+    const paymentCollection = client
+      .db("parcelService")
+      .collection("paymentHistory");
+    const trackingCollection = client
+      .db("parcelService")
+      .collection("trackingCollection");
 
     //get all parcel or user parcel by email
     app.get("/parcels", async (req, res) => {
@@ -46,6 +53,31 @@ async function run() {
       }
     });
 
+    //get parcel by id
+    app.get("/parcels/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const query = { _id: new ObjectId(id) };
+        const result = await parcelsCollection.findOne(query);
+        res.status(200).send(result);
+      } catch (error) {
+        console.error("Error fetching parcel:", error);
+        res.status(500).send({ message: "Error fetching parcel", error });
+      }
+    });
+
+    //get payment history
+    app.get("/payments", async (req, res) => {
+      const { email } = req.query;
+      let query = email ? { email: email } : {};
+
+      const paymentHistory = await paymentCollection
+        .find(query)
+        .sort({ paid_at: -1 })
+        .toArray();
+      res.status(200).send(paymentHistory);
+    });
+
     //post parcel from user
     app.post("/parcels", async (req, res) => {
       try {
@@ -56,6 +88,67 @@ async function run() {
         console.error("Error inserting parcel:", error);
         res.status(500).json({ message: "Error inserting parcel", error });
       }
+    });
+
+    //stripe payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { amount } = req.body;
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+
+        res.send({ clientSecret: paymentIntent.client_secret });
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    //payment history post
+    app.post("/payments", async (req, res) => {
+      const { parcelId, email, amount, paymentMethod, tnxId } = req.body;
+
+      const updateResult = await parcelsCollection.updateOne(
+        { _id: new ObjectId(parcelId) },
+        {
+          $set: {
+            payment_status: "paid",
+          },
+        }
+      );
+
+      const paymentHistoryEntry = {
+        parcelId,
+        email,
+        amount,
+        paymentMethod,
+        tnxId,
+        paid_at_string: new Date().toISOString(),
+        paid_at: new Date(),
+      };
+
+      const paymentResult = await paymentCollection.insertOne(
+        paymentHistoryEntry
+      );
+      res.status(200).send(paymentResult);
+    });
+
+    //update tracking parcel
+    app.post("/trackParcel", async (req, res) => {
+      const { tracking_id, parcel_id, status, message, updated_by } = req.body;
+      const newTracking = {
+        tracking_id,
+        parcel_id,
+        status,
+        message,
+        updated_by,
+        time: new Date(),
+      };
+
+      const insertResult = await trackingCollection.insertOne(newTracking);
+      return res.status(500).json(insertResult);
     });
 
     //delete parcel by its ID
